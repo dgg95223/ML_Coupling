806999d616b87866442b8c9b4862ce07f9e4337d
dgg95223 <deng.jing.heng223@hotmail.com>
2022-06-14 18:20:23 +0800

    6/14 update

---
+++ nn_frame.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+class NN(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.flatten = tf.keras.layers.Reshape(target_shape=(7 * 7 * 64,))
+        self.dense1 = tf.keras.layers.Dense(units=1024, activation=tf.nn.relu)
+        self.dense2 = tf.keras.layers.Dense(units=10)
+
+    def call(self, inputs):
+        x = self.flatten(x)                     # [batch_size, 7 * 7 * 64]
+        x = self.dense1(x)                      # [batch_size, 1024]
+        x = self.dense2(x)                      # [batch_size, 10]
+        output = tf.nn.softmax(x)
+        return output
