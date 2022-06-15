66110a453d25af04a4660625b796e45923dbf94d
dgg95223 <deng.jing.heng223@hotmail.com>
2022-06-15 19:05:08 +0800

    6/15 update

--- nn_frame.py
+++ nn_frame.py
@@ -1,11 +1,18 @@
 import tensorflow as tf
 
 class NN(tf.keras.Model):
-    def __init__(self):
+    def __init__(self, nn_shape=(5,5,10), batch_size=(1024,10), activation='tanh'):
         super().__init__()
-        self.flatten = tf.keras.layers.Reshape(target_shape=(7 * 7 * 64,))
-        self.dense1 = tf.keras.layers.Dense(units=1024, activation=tf.nn.relu)
-        self.dense2 = tf.keras.layers.Dense(units=10)
+        if activation == 'tanh':
+            self.activation = tf.nn.tanh
+        elif activation == 'relu':
+            self.activation = tf.nn.relu
+
+        self.nn_shape = nn_shape
+        self.batch_size = batch_size
+        self.flatten = tf.keras.layers.Reshape(target_shape=self.nn_shape)
+        self.dense1 = tf.keras.layers.Dense(units=self.batch_size[0], activation=self.activation)
+        self.dense2 = tf.keras.layers.Dense(units=self.batch_size[1])
 
     def call(self, inputs):
         x = self.flatten(x)                     # [batch_size, 7 * 7 * 64]
