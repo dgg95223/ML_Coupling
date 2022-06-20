fd62c68699fd752bead803b36de99140669bbedc
dgg95223 <deng.jing.heng223@hotmail.com>
2022-06-20 18:20:07 +0800

    6/20 updaate

--- nn_frame.py
+++ nn_frame.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
+import json
 
 class NN(tf.keras.Model):
     def __init__(self, nn_shape=(5,5,10), batch_size=(1024,10), activation='tanh'):
         super().__init__()
+        # initial load
+
+        # inital NN
         if activation == 'tanh':
             self.activation = tf.nn.tanh
         elif activation == 'relu':
@@ -14,9 +18,36 @@ class NN(tf.keras.Model):
         self.dense1 = tf.keras.layers.Dense(units=self.batch_size[0], activation=self.activation)
         self.dense2 = tf.keras.layers.Dense(units=self.batch_size[1])
 
-    def call(self, inputs):
-        x = self.flatten(x)                     # [batch_size, 7 * 7 * 64]
-        x = self.dense1(x)                      # [batch_size, 1024]
-        x = self.dense2(x)                      # [batch_size, 10]
+        # initial train
+
+        # initial test
+
+    def build_data_set(self):
+        file_name = self.file_name
+        
+        
+
+    def build_NN(self, inputs):
+        x = self.flatten(inputs)                     
+        x = self.dense1(x)                      
+        x = self.dense2(x)                      
         output = tf.nn.softmax(x)
         return output
+
+    def train(self):
+        model = self.call()
+        optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
+        num_batches = int(data_loader.num_train_data // args.batch_size * args.num_epochs)
+        checkpoint = tf.train.Checkpoint(myAwesomeModel=model)      # 实例化Checkpoint，设置保存对象为model
+        for batch_index in range(1, num_batches+1):                 
+            X, y = data_loader.get_batch(args.batch_size)
+            with tf.GradientTape() as tape:
+                y_pred = model(X)
+                loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=y, y_pred=y_pred)
+                loss = tf.reduce_mean(loss)
+                print("batch %d: loss %f" % (batch_index, loss.numpy()))
+            grads = tape.gradient(loss, model.variables)
+            optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
+            if batch_index % 100 == 0:                              # 每隔100个Batch保存一次
+                path = checkpoint.save('./save/model.ckpt')         # 保存模型参数到文件
+                print("model saved to %s" % path)
